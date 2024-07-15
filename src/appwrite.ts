5ddad01d5650a35c003c1941263b55d4e38c26b7
ni3rav <mahetan35@gmail.com>
2024-07-16 00:40:23 +0530

    setting up appwrite

---
+++ src/appwrite.ts
@@ -0,0 +1,13 @@
+import { Client, Account, Databases, Storage } from 'appwrite';
+
+const client = new Client();
+
+client
+    .setEndpoint(import.meta.env.VITE_APPWRITE_ENDPOINT as string)
+    .setProject(import.meta.env.VITE_APPWRITE_PROJECT_ID as string)
+
+const account = new Account(client);
+const databases = new Databases(client);
+const storage = new Storage(client);
+
+export { client, account, databases, storage };
